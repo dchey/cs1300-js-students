9fb0f73e020a8702e290361362fcb9d326e726c3
Daniel Chey <dong_keun_chey@brown.edu>
2020-11-02 01:38:14 +0900

    done

--- app.js
+++ app.js
@@ -25,7 +25,37 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+
+      // access JSON data
+      var data = JSON.parse(this.response).data;
+
+      //function
+      data.forEach(getPlants)
+
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+function getPlants(plant) {
+  var div = document.createElement('div')
+  div.className = "item";
+
+  //get images
+  var url = plant.image_url
+  if (url !== null) {
+    var img = document.createElement('img'); 
+    img.src = url;
+    div.appendChild(img);
+  }
+
+  //adding plant common names
+  var tag = document.createElement("tag");
+  tag.className = "text"
+  var name = document.createTextNode(plant.common_name);
+  tag.appendChild(name);
+  div.appendChild(tag);
+
+  //add to div w id=outputs in html
+  var element = document.getElementById("outputs");
+  element.appendChild(div);
+} 
